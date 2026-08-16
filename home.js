df04cb4606f888445ee7c3fc9e7c21bac016fe62
orangajaa <azimanmichael3@gmail.com>
2026-08-16 11:41:52 +0700

    update

--- home.js
+++ home.js
@@ -1,3 +1,15 @@
-function toggleMenu() {
-    document.querySelector(".Home").classList.toggle("active");
+function openPopup() {
+    document.getElementById('popupOverlay').classList.add('active');
+    document.getElementById('mainWrapper').classList.add('blurred');
+}
+
+function closePopup() {
+    document.getElementById('popupOverlay').classList.remove('active');
+    document.getElementById('mainWrapper').classList.remove('blurred');
+}
+
+function closePopupOnOverlay(event) {
+    if (event.target.id === 'popupOverlay') {
+        closePopup();
+    }
 }
